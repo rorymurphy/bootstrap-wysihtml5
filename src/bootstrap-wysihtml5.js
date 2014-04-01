60a1f509999518d1fd071f3d013b3c9068ea8cdf
Rory <ndmurph04@gmail.com>
2014-03-31 22:21:27 -0400

    Updated to use AMD wrapper

--- src/bootstrap-wysihtml5.js
+++ src/bootstrap-wysihtml5.js
@@ -1,4 +1,12 @@
-!function($, wysi) {
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define(['jquery', 'wysihtml5'], factory);
+    } else {
+        // Browser globals
+        root.amdWeb = factory(root.jQuery, root.wysihtml5);
+    }
+}(this, function ($, wysi) {
     "use strict";
 
     var tpl = {
@@ -508,4 +516,5 @@
         }
     };
 
-}(window.jQuery, window.wysihtml5);
+    return {};
+}));
